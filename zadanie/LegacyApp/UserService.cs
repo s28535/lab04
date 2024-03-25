fadfddb1861f9f728d2b6236eb5b050bbeb29d6a
s28535 <s28535@pjwstk.edu.pl>
2024-03-25 13:28:35 +0100

    second commit

--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -4,26 +4,29 @@ namespace LegacyApp
 {
     public class UserService
     {
-        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        public bool IsDataCorrect(string firstName, string lastName, string email)
         {
             if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
-            {
                 return false;
-            }
+            return !email.Contains("@") && !email.Contains(".");
+        }
 
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-
+        public int CalculateAge(DateTime dateOfBirth)
+        {
             var now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
             if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
+            return age;
+        }
+        
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (IsDataCorrect(firstName, lastName, email))
                 return false;
-            }
+
+            int age = CalculateAge(dateOfBirth);
+            if (age < 21)
+                return false;
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);

6287d0760f37f3b63727ea07ae009d725e9145c1
mkostana <milosz.kostana@gmail.com>
2024-04-08 11:26:30 +0200

    refacotr userService

--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -4,19 +4,49 @@ namespace LegacyApp
 {
     public class UserService
     {
-        public bool IsDataCorrect(string firstName, string lastName, string email)
+        private bool IsDataCorrect(string firstName, string lastName, string email)
         {
             if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
                 return false;
             return !email.Contains("@") && !email.Contains(".");
         }
 
-        public int CalculateAge(DateTime dateOfBirth)
+        private int CalculateAge(DateTime dateOfBirth)
         {
             var now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
             if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
             return age;
+        }
+
+        private void CalculateCreditLimit(User user)
+        {
+            var clientType = user.Client.Type;
+
+            using (var userCreditService = new UserCreditService())
+            {
+                int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+
+                switch (clientType)
+                {
+                    case "VeryImportantClient":
+                        user.HasCreditLimit = false;
+                        break;
+                    case "ImportantClient":
+                        creditLimit = creditLimit * 2;
+                        SetCreditLimit(user, creditLimit);
+                        break;
+                    default:
+                        SetCreditLimit(user, creditLimit);
+                        break;
+                }
+            }
+        }
+        
+        private void SetCreditLimit(User user, int creditLimit)
+        {
+            user.HasCreditLimit = true;
+            user.CreditLimit = creditLimit;
         }
         
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
@@ -40,33 +70,10 @@ namespace LegacyApp
                 LastName = lastName
             };
 
-            if (client.Type == "VeryImportantClient")
-            {
-                user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
-            else
-            {
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
-            }
+            CalculateCreditLimit(user);
 
             if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
                 return false;
-            }
 
             UserDataAccess.AddUser(user);
             return true;
